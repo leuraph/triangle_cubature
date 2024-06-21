5a150abd4d47679c751a0988258c3781d79abbe1
rleu <raphaelleu95@gmail.com>
2024-06-21 11:52:16 +0200

    chore: implemented passing `transform_integration_points`

--- src/triangle_cubature/transformations.py
+++ src/triangle_cubature/transformations.py
@@ -2,18 +2,27 @@ import numpy as np
 from p1afempy.data_structures import CoordinatesType
 
 
-def transform_weights(
-        physical_triangle: CoordinatesType,
-        reference_weights: np.ndarray) -> np.ndarray:
-    # TODO implement
-    return np.zeros_like(reference_weights)
+def transform_weights(physical_triangle: CoordinatesType,
+                      reference_weights: np.ndarray) -> np.ndarray:
+    jacobian_determinant = np.linalg.det(
+        get_jacobian(physical_triangle=physical_triangle)
+    )
+    return jacobian_determinant * reference_weights
 
 
 def transform_integration_points(
         physical_triangle: CoordinatesType,
         reference_integration_points: CoordinatesType) -> CoordinatesType:
-    # TODO implement
-    return np.zeros_like(reference_integration_points)
+    p1 = physical_triangle[0, :]
+    jacobian = get_jacobian(physical_triangle=physical_triangle)
+
+    transformed_integration_points = []
+    for reference_integration_point in reference_integration_points:
+        transformed_integration_point = p1 + jacobian.dot(
+            reference_integration_point)
+        transformed_integration_points.append(transformed_integration_point)
+
+    return np.array(transformed_integration_points)
 
 
 def get_jacobian(physical_triangle: CoordinatesType) -> np.ndarray:
