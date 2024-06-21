aee73708989ac93981885e37afc23f893bf2c7db
rleu <raphaelleu95@gmail.com>
2024-06-21 11:11:42 +0200

    style

--- src/triangle_cubature/integrate.py
+++ src/triangle_cubature/integrate.py
@@ -1,5 +1,6 @@
 from p1afempy.data_structures import CoordinatesType
-from triangle_cubature.weights_and_integration_points import WeightsAndIntegrationPoints
+from triangle_cubature.weights_and_integration_points \
+    import WeightsAndIntegrationPoints
 
 
 def integrate_on_triangle(
