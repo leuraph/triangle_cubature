66549c3b0960d9d7cf959fa610de1402ef78afdc
rleu <raphaelleu95@gmail.com>
2024-06-21 12:58:06 +0200

    refactor: improved /fixed signature of cubature functions

--- src/triangle_cubature/integrate.py
+++ src/triangle_cubature/integrate.py
@@ -1,17 +1,20 @@
-from p1afempy.data_structures import CoordinatesType
+from p1afempy.data_structures import CoordinatesType, ElementsType
 from triangle_cubature.weights_and_integration_points \
     import WeightsAndIntegrationPoints
 
 
 def integrate_on_triangle(
-        coordinates: CoordinatesType,
+        f: callable,
+        triangle: CoordinatesType,
         rule: WeightsAndIntegrationPoints) -> float:
     # TODO IMPLEMENT
     return 0.
 
 
 def integrate_on_mesh(
+        f: callable,
         coordinates: CoordinatesType,
+        elements: ElementsType,
         rule: WeightsAndIntegrationPoints) -> float:
     sum = 0.
     for triangle in coordinates:
