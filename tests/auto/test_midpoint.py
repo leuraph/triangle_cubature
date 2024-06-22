4b73438f621c97e0753f68bfce5161ca1ee0112f
rleu <raphaelleu95@gmail.com>
2024-06-22 11:15:07 +0200

    chore: added todos

--- tests/auto/test_midpoint.py
+++ tests/auto/test_midpoint.py
@@ -3,7 +3,8 @@ import unittest
 
 class TestMidpoint(unittest.TestCase):
     def test_midpoint(self):
-        # TODO implement unittest
+        # TODO generate random triangles and integrate separately a constant function
+        # TODO generate a mesh, refine it with NVB and integrate a constant function
         pass
 
 
