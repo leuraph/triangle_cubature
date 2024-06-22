1806515b2d955d882b4dbd9b2c8faab5180d4679
rleu <raphaelleu95@gmail.com>
2024-06-22 11:13:39 +0200

    test: added shallow `TestMidpoint`

---
+++ tests/auto/test_midpoint.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestMidpoint(unittest.TestCase):
+    def test_midpoint(self):
+        # TODO implement unittest
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
