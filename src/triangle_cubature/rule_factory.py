3546af3456c178da998b23ac0ffe8ba43a414c63
rleu <raphaelleu95@gmail.com>
2024-06-22 16:24:59 +0200

    bugfix: typo in `rule_factory.get_rule`

--- src/triangle_cubature/rule_factory.py
+++ src/triangle_cubature/rule_factory.py
@@ -12,5 +12,5 @@ def get_rule(rule: CubatureRule) -> WeightsAndIntegrationPoints:
     if rule == CubatureRule.MIDPOINT:
         return WeightsAndIntegrationPoints(
             weights=np.array([1./2.]),
-            integration_points=np.array[[1./3., 1./3.]]
+            integration_points=np.array([1./3., 1./3.])
         )
