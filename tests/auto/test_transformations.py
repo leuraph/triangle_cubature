d97d224a0d65e34ad3600d21cc7310d5531aef73
rleu <raphaelleu95@gmail.com>
2024-06-22 11:10:16 +0200

    chore: restored unittests for transformations of weights and integration points

--- tests/auto/test_transformations.py
+++ tests/auto/test_transformations.py
@@ -1,5 +1,6 @@
 import unittest
-from triangle_cubature.transformations import get_jacobian, \
+from triangle_cubature.transformations \
+    import transform_weights, transform_integration_points, get_jacobian, \
     transform_weights_and_integration_points
 import numpy as np
 from triangle_cubature.weights_and_integration_points \
@@ -7,6 +8,39 @@ from triangle_cubature.weights_and_integration_points \
 
 
 class TestTransformations(unittest.TestCase):
+    def test_transform_weights(self):
+        reference_weights = np.array([1.])
+        physical_coordinates = np.array([
+            [0., 0.],
+            [1., 0.],
+            [0., 1.]
+        ])
+        jacobian = get_jacobian(physical_triangle=physical_coordinates)
+        transformed_weights = transform_weights(
+            reference_weights=reference_weights,
+            jacobian=jacobian)
+        self.assertTrue(
+            np.allclose(reference_weights, transformed_weights))
+
+    def test_transform_coordinates(self):
+        reference_coordinates = np.array([
+            [0.0, 0.0],
+            [1.0, 0.0],
+            [0.0, 1.0],
+            [1./3., 1./3.]
+        ])
+        physical_coordinates = np.array([
+            [0., 0.],
+            [1., 0.],
+            [0., 1.]
+        ])
+        jacobian = get_jacobian(physical_triangle=physical_coordinates)
+        transformed_coordinates = transform_integration_points(
+            reference_integration_points=reference_coordinates,
+            p1=physical_coordinates[0, :],
+            jacobian=jacobian)
+        self.assertTrue(
+            np.allclose(reference_coordinates, transformed_coordinates))
 
     def transform_weights_and_integration_points(self):
         # -------------------------------------------------------------
