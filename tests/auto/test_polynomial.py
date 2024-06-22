2d53050330c2ef619bce488abecd2769dd68018f
rleu <raphaelleu95@gmail.com>
2024-06-22 16:52:48 +0200

    chore: added shallow polynomial unittest

---
+++ tests/auto/test_polynomial.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestPolynomial(unittest.TestCase):
+    def test_monomials():
+        pass
+
+    def test_polynomials():
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
