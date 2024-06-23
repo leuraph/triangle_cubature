f252e76c424717d4ed018279af43bec54a98e85e
rleu <raphaelleu95@gmail.com>
2024-06-23 23:20:13 +0200

    chore: implemented `integrate_on_triangle`

--- src/triangle_cubature/integrate.py
+++ src/triangle_cubature/integrate.py
@@ -1,18 +1,23 @@
-from p1afempy.data_structures import CoordinatesType, ElementsType
+from p1afempy.data_structures import \
+    CoordinatesType, ElementsType, BoundaryConditionType
 from triangle_cubature.weights_and_integration_points \
     import WeightsAndIntegrationPoints
+from triangle_cubature.transformations import \
+    transform_weights_and_integration_points
+import numpy as np
 
 
 def integrate_on_triangle(
-        f: callable,
-        triangle: CoordinatesType,
+        f: BoundaryConditionType, triangle: CoordinatesType,
         rule: WeightsAndIntegrationPoints) -> float:
-    # TODO IMPLEMENT
-    return 0.
+    transformed = transform_weights_and_integration_points(
+        weights_and_integration_points=rule,
+        physical_triangle=triangle)
+    return np.dot(transformed.weights, f(transformed.integration_points))
 
 
 def integrate_on_mesh(
-        f: callable,
+        f: BoundaryConditionType,
         coordinates: CoordinatesType,
         elements: ElementsType,
         rule: WeightsAndIntegrationPoints) -> float:
