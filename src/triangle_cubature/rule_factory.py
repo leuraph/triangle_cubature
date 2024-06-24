6302a3d59218d21a252ecba9e847567e8b05c3dc
rleu <raphaelleu95@gmail.com>
2024-06-24 10:25:25 +0200

    doc: docstring for `rule_factory.get_rule`

--- src/triangle_cubature/rule_factory.py
+++ src/triangle_cubature/rule_factory.py
@@ -10,6 +10,21 @@ class CubatureRule(Enum):
 
 
 def get_rule(rule: CubatureRule) -> WeightsAndIntegrationPoints:
+    """
+    given a cubature rule, returns the corresponding
+    weight(s) and integration point(s)
+
+    Notes
+    -----
+    - the rules correspond to the rules as specified in [1]
+
+    References
+    ----------
+    - [1] Stenger, Frank.
+      'Approximate Calculation of Multiple Integrals (A. H. Stroud)'.
+      SIAM Review 15, no. 1 (January 1973): 234-35.
+      https://doi.org/10.1137/1015023. p. 306-315
+    """
     if rule == CubatureRule.MIDPOINT:
         return WeightsAndIntegrationPoints(
             weights=np.array([1./2.]),
