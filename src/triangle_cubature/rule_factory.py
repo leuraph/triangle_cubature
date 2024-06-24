76af3980a6e3c1cd79fef42d66e99c0c64452e72
rleu <raphaelleu95@gmail.com>
2024-06-24 10:18:16 +0200

    chore: implemented lauffer cubature + unit test

--- src/triangle_cubature/rule_factory.py
+++ src/triangle_cubature/rule_factory.py
@@ -6,6 +6,7 @@ from triangle_cubature.weights_and_integration_points \
 
 class CubatureRule(Enum):
     MIDPOINT = 1
+    LAUFFER = 2
 
 
 def get_rule(rule: CubatureRule) -> WeightsAndIntegrationPoints:
@@ -14,3 +15,18 @@ def get_rule(rule: CubatureRule) -> WeightsAndIntegrationPoints:
             weights=np.array([1./2.]),
             integration_points=np.array([1./3., 1./3.]).reshape(1, 2)
         )
+    if rule == CubatureRule.LAUFFER:
+        integration_points = np.array([
+            [0., 0.],
+            [1., 0.],
+            [0., 1.]
+        ])
+        weights = np.array([
+            1/3 * 0.5,
+            1/3 * 0.5,
+            1/3 * 0.5
+        ])
+        return WeightsAndIntegrationPoints(
+            integration_points=integration_points,
+            weights=weights)
+    raise ValueError('specified rule does not exist.')
--- tests/auto/test_cubature_rules.py
+++ tests/auto/test_cubature_rules.py
@@ -35,6 +35,35 @@ class TestCubatureRules(unittest.TestCase):
 
         # TODO test on mesh
 
+    def test_lauffer(self):
+        # -----
+        # setup
+        # -----
+        lauffer_rule = triangle_cubature.rule_factory.get_rule(
+            rule=triangle_cubature.rule_factory.CubatureRule.LAUFFER)
+        np.random.seed(42)
+
+        # -----------------------------------------------------
+        # LAUFFER must have exactness order of degree 1, i.e.
+        # polynomials must be integrated exactly up to degree 1
+        # -----------------------------------------------------
+        n_tests = 5
+        for _ in range(n_tests):
+            random_triangle = utils.generate_random_triangle()
+            random_linear_polynomial = polynomials.get_random_polynomial(
+                degree=1)
+            calculated_result = \
+                triangle_cubature.integrate.integrate_on_triangle(
+                    f=random_linear_polynomial.eval_at,
+                    triangle=random_triangle,
+                    rule=lauffer_rule)
+            expected_result = polynomials.integrate_on_triangle(
+                polynomial=random_linear_polynomial,
+                vertices=random_triangle)
+            self.assertAlmostEqual(calculated_result, expected_result)
+
+        # TODO test on mesh
+
 
 if __name__ == '__main__':
     unittest.main()
