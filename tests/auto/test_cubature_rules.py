cd86a25b1cf747db5fca45db0c25234dbe147a6f
rleu <raphaelleu95@gmail.com>
2024-06-24 11:48:04 +0200

    refactor: added generic unit test testing all cubature rules in the same way

--- tests/auto/test_cubature_rules.py
+++ tests/auto/test_cubature_rules.py
@@ -7,6 +7,8 @@ from p1afempy.io_helpers import read_mesh, read_boundary_condition
 from p1afempy.refinement import refineNVB
 from pathlib import Path
 from tqdm import tqdm
+from triangle_cubature.rule_factory import CubatureRule
+from triangle_cubature.rule_factory import get_rule
 
 
 class TestCubatureRules(unittest.TestCase):
@@ -144,6 +146,84 @@ class TestCubatureRules(unittest.TestCase):
             elements=elements,
             weights_and_integration_points=lauffer_rule.weights_and_integration_points)
         self.assertAlmostEqual(expected_result, calculated_result)
+
+    def test_cubature_rules(self):
+        # -----
+        # setup
+        # -----
+        rules_to_test: list[CubatureRule] = [
+            triangle_cubature.rule_factory.CubatureRuleEnum.MIDPOINT,
+            triangle_cubature.rule_factory.CubatureRuleEnum.LAUFFER
+        ]
+
+        # reading and refining a mesh (same for all tests)
+        base_path = Path('tests/data/simple_square_mesh/')
+        path_to_coordinates = base_path / Path('coordinates.dat')
+        path_to_elements = base_path / Path('elements.dat')
+        path_to_boundary = base_path / Path('boundary.dat')
+
+        coordinates, elements = read_mesh(
+            path_to_coordinates=path_to_coordinates,
+            path_to_elements=path_to_elements)
+        # there's only one boundary
+        boundaries = [read_boundary_condition(
+            path_to_boundary=path_to_boundary)]
+
+        n_refinements = 2
+        for _ in range(n_refinements):
+            marked_elements = np.arange(elements.shape[0])
+            coordinates, elements, boundaries, _ = refineNVB(
+                coordinates=coordinates,
+                elements=elements,
+                marked_elements=marked_elements,
+                boundary_conditions=boundaries
+            )
+
+        # -----
+        # TESTS
+        # -----
+        np.random.seed(42)
+
+        for cubature_rule in [get_rule(rule) for rule in rules_to_test]:
+
+            # test on single triangle
+            print(
+                f'unit testing {cubature_rule.name}-cubature'
+                ' on single triangles...')
+            n_tests = 5
+            for _ in tqdm(range(n_tests)):
+                random_triangle = utils.generate_random_triangle()
+                random_polynomial = polynomials.get_random_polynomial(
+                    degree=cubature_rule.degree_of_exactness)
+                calculated_result = \
+                    triangle_cubature.integrate.integrate_on_triangle(
+                        f=random_polynomial.eval_at,
+                        triangle=random_triangle,
+                        weights_and_integration_points=cubature_rule.weights_and_integration_points)
+                expected_result = polynomials.integrate_on_triangle(
+                    polynomial=random_polynomial,
+                    vertices=random_triangle)
+                self.assertAlmostEqual(calculated_result, expected_result)
+
+            # test on mesh
+            print(
+                f'unit testing {cubature_rule.name}-cubature'
+                ' on refined mesh...')
+
+            random_triangle = utils.generate_random_triangle()
+            random_polynomial = polynomials.get_random_polynomial(
+                degree=cubature_rule.degree_of_exactness)
+            expected_result = polynomials.integrate_on_mesh(
+                polynomial=random_polynomial,
+                elements=elements,
+                vertices=coordinates,
+                display_progress=True)
+            calculated_result = triangle_cubature.integrate.integrate_on_mesh(
+                f=random_polynomial.eval_at,
+                coordinates=coordinates,
+                elements=elements,
+                weights_and_integration_points=cubature_rule.weights_and_integration_points)
+            self.assertAlmostEqual(expected_result, calculated_result)
 
 
 if __name__ == '__main__':
