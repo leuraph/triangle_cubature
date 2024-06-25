f6c1229dc58ebeca9cc9eeeaccecaffa05745040
rleu <raphaelleu95@gmail.com>
2024-06-25 08:09:50 +0200

    doc

--- src/triangle_cubature/integrate.py
+++ src/triangle_cubature/integrate.py
@@ -1,16 +1,42 @@
 from p1afempy.data_structures import \
-    CoordinatesType, ElementsType, BoundaryConditionType
+    CoordinatesType, ElementsType
 from triangle_cubature.cubature_rule \
     import CubatureRuleEnum
 from triangle_cubature.transformations import \
     transform_weights_and_integration_points
 from triangle_cubature.rule_factory import get_rule
+from typing import Callable
 import numpy as np
 
 
 def integrate_on_triangle(
-        f: BoundaryConditionType, triangle: CoordinatesType,
+        f: Callable[[CoordinatesType], np.ndarray],
+        triangle: CoordinatesType,
         cubature_rule: CubatureRuleEnum) -> float:
+    """
+    approximates the integral of the function provided
+    on the triangle at hand using the specified cubature rule
+
+    parameters
+    ----------
+    f: Callable[[CoordinatesType], np.ndarray]
+        the function to be integrated
+    triangle: CoordinatesType
+        the coordinates of the triangle's vertices
+        in counter-clockwise order
+    cubature_rule: CubatureRuleEnum
+        the cubature rule to be used
+
+    returns
+    -------
+    float: the approximated value of the integral
+
+    notes
+    -----
+    - the function f must be able to
+      handle inputs of shape (N, 2), i.e.
+      coordinates as array
+    """
     waip = get_rule(rule=cubature_rule).weights_and_integration_points
     transformed = transform_weights_and_integration_points(
         weights_and_integration_points=waip,
@@ -19,10 +45,35 @@ def integrate_on_triangle(
 
 
 def integrate_on_mesh(
-        f: BoundaryConditionType,
+        f: Callable[[CoordinatesType], np.ndarray],
         coordinates: CoordinatesType,
         elements: ElementsType,
         cubature_rule: CubatureRuleEnum) -> float:
+    """
+    approximates the integral of the function provided
+    over the mesh at hand using the specified cubature rule
+
+    parameters
+    ----------
+    f: Callable[[CoordinatesType], np.ndarray]
+        the function to be integrated
+    coordinates: CoordinatesType
+        vrtices of the mesh
+    elements: ElementsType
+        the elements of the mesh
+    cubature_rule: CubatureRuleEnum
+        the cubature rule to be used
+
+    returns
+    -------
+    float: the approximated value of the integral
+
+    notes
+    -----
+    - the function f must be able to
+      handle inputs of shape (N, 2), i.e.
+      coordinates as array
+    """
     sum = 0.
     for triangle in elements:
         sum += integrate_on_triangle(
